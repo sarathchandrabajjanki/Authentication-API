5bd5e61c7ee796a93b959dfebbec9c4210d47097
sarathchandrabajjanki <sarathchandrabajjanki@gmail.com>
2025-10-14 12:00:08 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express=require("express")
 const ConnectToDataBase = require("./DataBase/DB")
 const app=express()
 var cors=require("cors")
-var Route=require("./Routes/Auth-Route")
+var Route=require("./Routes/User-Route")
+var HomeRoute=require("./Routes/home-Route")
 //connect to data base
 
 ConnectToDataBase()
@@ -11,6 +12,8 @@ ConnectToDataBase()
 // added Middle Ware
 app.use(express.json())
 app.use("/api",Route)
+app.use("/api/",HomeRoute)
+
 app.use(cors())
 
 
